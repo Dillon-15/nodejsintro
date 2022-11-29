3af0fe8a6cd84d7bd407b2ed20cf47994391db82
BARNETT <db0958392@otc.edu>
2022-11-29 08:49:33 -0600

    added readme file

--- apps.js
+++ apps.js
@@ -1,1 +1,8 @@
+// how to use git
+// git init
+// git add *
+// git commit -m "first commit"
+// paste the code from github to add it to the repository
+// paste in git push -u origin main
+
 console.log("I am so Giddy today about Octopi!!")
